2763a4720ba15a74f03e46f3078a8828d42d9c2e
Tomáš Jelínek <tjelinek@komtesa.com>
2017-11-06 19:07:46 +0100

    mista

--- web/js/map.js
+++ web/js/map.js
@@ -56,4 +56,78 @@ function initMap() {
         position: {lat: 49.706330, lng: 15.734276},
         map: map,
     });
+    var strakonickaPraha = new google.maps.Marker({
+        position: {lat: 49.996732, lng: 14.396787},
+        map: map,
+    });
+    var modranskaPraha = new google.maps.Marker({
+        position: {lat: 50.022772, lng: 14.404118},
+        map: map,
+    });
+    var modranskaPrahaZpet = new google.maps.Marker({
+        position: {lat: 50.024072, lng: 14.404077},
+        map: map,
+    });
+    var jizniSpojkaPraha = new google.maps.Marker({
+        position: {lat: 50.032572, lng: 14.422992},
+        map: map,
+    });
+    var jizniSpojkaPrahaZpet = new google.maps.Marker({
+        position: {lat: 50.032823, lng: 14.426359},
+        map: map,
+    });
+    var zalesiPraha = new google.maps.Marker({
+        position: {lat: 50.024396, lng: 14.442256},
+        map: map,
+    });
+    var zalesiPrahaZpet = new google.maps.Marker({
+        position: {lat: 50.024546, lng: 14.442309},
+        map: map,
+    });
+    var patehoKvetnaPraha = new google.maps.Marker({
+        position: {lat: 50.042104, lng: 14.464861},
+        map: map,
+    });
+    var jizniSpojkaOkruhPraha = new google.maps.Marker({
+        position: {lat: 50.048451, lng: 14.474146},
+        map: map,
+    });
+    var turkovaPraha = new google.maps.Marker({
+        position: {lat: 50.041470, lng: 14.488477},
+        map: map,
+    });
+    var hrbitovPraha = new google.maps.Marker({
+        position: {lat: 50.062415, lng: 14.469517},
+        map: map,
+    });
+    var prumyslovaPraha = new google.maps.Marker({
+        position: {lat: 50.099203, lng: 14.531047},
+        map: map,
+    });
+    var prumyslovaPrahaZpet = new google.maps.Marker({
+        position: {lat: 50.101791, lng: 14.533085},
+        map: map,
+    });
+    var podebradskaPraha = new google.maps.Marker({
+        position: {lat: 50.106624, lng: 14.542549},
+        map: map,
+    });
+    var vHolesovickachPraha = new google.maps.Marker({
+        position: {lat: 50.117477, lng: 14.542549},
+        map: map,
+    });
+    var jeremenkovaPraha = new google.maps.Marker({
+        position: {lat: 50.046984, lng: 14.414113},
+        map: map,
+    });
+    var dobrizskaPraha = new google.maps.Marker({
+        position: {lat: 50.058039, lng: 14.406759},
+        map: map,
+    });
+    var bieblovaPraha = new google.maps.Marker({
+        position: {lat: 50.058039, lng: 14.406759},
+        map: map,
+    });
+
+
 }
